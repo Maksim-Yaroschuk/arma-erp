aeafdc536423e6770df8b66e0ae30845f38ed75a
Maksim-Yaroschuk <79583144+Maksim-Yaroschuk@users.noreply.github.com>
2022-10-15 21:23:15 +0300

    add reading xlsx files

--- src/components/Logistics/Logistics.jsx
+++ src/components/Logistics/Logistics.jsx
@@ -1,7 +1,26 @@
+import { Form, Label } from 'components/GlobalStyle';
+import readXlsxFile from 'read-excel-file';
+
 export const Logistics = () => {
+  const handleFiles = e => {
+    const file = e.target.files[0];
+    console.log(file);
+    readXlsxFile(file).then((rows) => {
+      console.log(rows);
+    });
+    return file;
+  };
+
   return (
     <div>
       <h3>Розділ логістика</h3>
+
+      <Form>
+        <Label>
+          Оберіть файл для імпорту
+          <input type="file" onChange={handleFiles}></input>
+        </Label>
+      </Form>
     </div>
   );
 };
