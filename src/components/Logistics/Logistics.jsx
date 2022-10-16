51a9d912860bbdb07a9002087f799f166c14b3c7
Maksim-Yaroschuk <79583144+Maksim-Yaroschuk@users.noreply.github.com>
2022-10-16 21:26:47 +0300

    render table from the file

--- src/components/Logistics/Logistics.jsx
+++ src/components/Logistics/Logistics.jsx
@@ -1,15 +1,21 @@
 import { Form, Label } from 'components/GlobalStyle';
+import { useState } from 'react';
 import readXlsxFile from 'read-excel-file';
 
 export const Logistics = () => {
+  const [head, setHead] = useState([]);
+  const [data, setData] = useState([]);
   const handleFiles = e => {
     const file = e.target.files[0];
-    console.log(file);
-    readXlsxFile(file).then((rows) => {
-      console.log(rows);
+    // console.log(file);
+    readXlsxFile(file).then(rows => {
+      // console.log(rows);
+      setHead(rows[0]);
+      setData(rows.slice(1));
     });
-    return file;
   };
+
+  console.log(data);
 
   return (
     <div>
@@ -21,6 +27,30 @@ export const Logistics = () => {
           <input type="file" onChange={handleFiles}></input>
         </Label>
       </Form>
+
+      {head.length && (
+        <table border="2" bordercolor="black">
+          <caption>Продажі</caption>
+          <thead>
+            <tr>
+              {head.map(row => {
+                return <td>{row}</td>;
+              })}
+            </tr>
+          </thead>
+          <tbody>
+            {data.map(rows => {
+              return (
+                <tr>
+                  {rows.map(row => {
+                    return <td>{row}</td>;
+                  })}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
