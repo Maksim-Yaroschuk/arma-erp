e94e3c0f0d3489039a8071346f29ccfb7cc362c2
Maksim-Yaroschuk <79583144+Maksim-Yaroschuk@users.noreply.github.com>
2022-10-16 20:50:52 +0300

    Revert "add reading xlsx files"
    This reverts commit aeafdc536423e6770df8b66e0ae30845f38ed75a.

--- src/components/Logistics/Logistics.jsx
+++ src/components/Logistics/Logistics.jsx
@@ -1,22 +1,15 @@
 import { Form, Label } from 'components/GlobalStyle';
-import { scryRenderedComponentsWithType } from 'react-dom/test-utils';
-import { useDispatch, useSelector } from 'react-redux';
 import readXlsxFile from 'read-excel-file';
-import { setFileData } from 'redux/importData/salesSlice';
-import { selectData } from 'redux/importData/selectors';
 
 export const Logistics = () => {
-  const dispatch = useDispatch();
   const handleFiles = e => {
-    dispatch(setFileData(e.target.files[0]));
-    // const file = e.target.files[0];
-    // console.log(file);
-    // readXlsxFile(file).then((rows) => {
-    //   console.log(rows);
-    // });
-    // return file;
+    const file = e.target.files[0];
+    console.log(file);
+    readXlsxFile(file).then((rows) => {
+      console.log(rows);
+    });
+    return file;
   };
-  const data = useSelector(selectData);
 
   return (
     <div>
@@ -28,18 +21,6 @@ export const Logistics = () => {
           <input type="file" onChange={handleFiles}></input>
         </Label>
       </Form>
-      {data && <table>
-        <caption>Продажі</caption>
-        <thead>
-          <tr>
-            {data.map(rows => {
-              return rows[0].map(row => {
-                return <td>{row}</td>;
-              });
-            })}
-          </tr>
-        </thead>
-      </table>}
     </div>
   );
 };
